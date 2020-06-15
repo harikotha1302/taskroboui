f838bccb4a36b8dad7e98ad550132d84087db178
prashanthi paladugu <prashanthipaladugu@prashanthis-MacBook-Pro.local>
2020-06-15 16:51:04 +0530

    Intital Commit to UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
